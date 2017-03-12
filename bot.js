20ff6ec6f957311e47b2329fdbe1765a3437a416
fmasroor <faraz_masroor@yahoo.com>
2017-03-12 16:52:27 -0400

    s
    s

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = cool() + ' ' + botID;
 
   options = {
     hostname: 'api.groupme.com',
